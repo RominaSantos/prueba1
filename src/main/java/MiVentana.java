de1c073bd12b178c55f7bbd81b03381ca0365706
romin <romin@192.168.1.95>
2020-10-09 13:55:58 -0500

    1.2

---
+++ src/main/java/MiVentana.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author romin
+ */
+import javax.swing.*;
+
+public class MiVentana extends JFrame {
+    public MiVentana () {
+        super("Mi otra ventana");
+        setSize(500, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}

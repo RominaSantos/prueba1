4e3bda141c216d483b291dc28d12c13171def9c9
romin <romin@192.168.1.95>
2020-10-09 13:40:19 -0500

    1.1

---
+++ src/main/java/VentanaPrueba.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author romin
+ */
+import javax.swing.*;
+
+public class VentanaPrueba {
+    public static void main(atring[] arg) {
+        JFrame f = new JFrame("Mi ventana");
+        f.setSize(400, 300);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
